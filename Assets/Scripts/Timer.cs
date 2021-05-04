12a092da5ebe75ffcddf4e41d67df31f20e47b5d
SolisSaturnus <77696391+SolisSaturnus@users.noreply.github.com>
2021-05-04 16:42:58 -0500

    Final-Exam
    Final Exam repo

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    public float timeRemaining;
+    public Text timeLeft;
+
+
+    void Start()
+    {
+        timeRemaining = PlayerPrefs.GetFloat("time");
+
+
+        timeLeft.text = "time remaining: " + timeRemaining.ToString("F2");
+    }
+
+
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining >= 0)
+        {
+            timeLeft.text = "time remaining: " + timeRemaining.ToString("F2");
+        }
+        else
+        {
+            SceneManager.LoadScene("3Exit");
+        }
+    }
+}
